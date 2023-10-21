b5e672615a4e8b2e58931c71332a7347cf8df12f
iro claire <iroclairec@gmail.com>
2023-10-21 19:57:23 +0100

    message

---
+++ index.js
@@ -0,0 +1,19 @@
+// const data = ()=>{
+//     fetch ("https://swapi.dev/api/people/1")
+//  .then (people => people.json())
+//  .then (converted => console.log (converted))
+ 
+//  const people =document.querySelector(".user")
+//     container.forEach((user)=>{
+//        people.innerHTML += `
+//     <div>
+//        <h1>${people.name}</h1>
+//        <h1>${people.gender}</h1>
+//     </div>
+//  `;
+ 
+    
+// })
+// }
+// data()
+
